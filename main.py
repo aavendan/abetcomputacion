2d4b2d679d9c83290bfd359fda010a037d840c2a
Allan <aavendan@gmail.com>
2019-01-13 16:42:07 -0500

    on flask

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+   return render_template('index.html')
+
+@app.route('/formacion')
+def formacion():
+   return render_template('formacion.html')
+
+if __name__ == '__main__':
+	app.debug = True
+	app.run()
