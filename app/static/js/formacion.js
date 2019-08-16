75e0f82c29707ae17f67cbdbd00753b463d042b4
Allan <aavendan@gmail.com>
2019-08-15 23:34:59 -0500

    improvement & split multilines

--- app/static/js/formacion.js
+++ app/static/js/formacion.js
@@ -27,16 +27,31 @@ $(document).ready(function() {
                     $('#ras_sp').empty()
 
                     template = `
-                 <div class="alert alert-success" role="alert">
-                  %text%
-                </div>
-              `
+                         <div class="alert alert-success" role="alert">
+                          %text%
+                        </div>
+                      `
 
                     response.forEach((item) => {
-
-                        $('#ras_en').append(template.replace('%text%', item.en))
-                        $('#ras_sp').append(template.replace('%text%', item.sp))
+                        if (item.en.length > 4) {
+                            $('#ras_en').append(template.replace('%text%', item.en))
+                        }
+                        if (item.sp.length > 4) {
+                            $('#ras_sp').append(template.replace('%text%', item.sp))
+                        }
                     })
+
+                    if ($('#ras_en').children().length > 0) {
+                        $('#hasEn').show()
+                    } else {
+                        $('#hasEn').hide()
+                    }
+
+                    if ($('#ras_sp').children().length > 0) {
+                        $('#hasSp').show()
+                    } else {
+                        $('#hasSp').hide()
+                    }
 
                 });
             //
@@ -56,34 +71,64 @@ $(document).ready(function() {
 
                     $('#rec_form').empty()
                     $('#rec_at').empty()
+                    $('#improvement').empty()
 
                     template = `
-                 <div class="alert alert-info" role="alert">
-                  %text%
-                </div>
-              `
+                         <div class="alert %alert_type%" role="alert">
+                          %text%
+                        </div>
+                      `
 
                     response.forEach((item) => {
 
                         if (item.formation.length > 4) {
-                            $('#rec_form').append(template.replace('%text%', item.formation))
+                            let formation = item.formation;
+                            let formations = formation.split("\n").filter(function(el) {
+                                return el.length > 0;
+                            });
+
+                            formations.forEach((formation) => {
+                                $('#rec_form').append(template
+                                    .replace('%text%', formation)
+                                    .replace('%alert_type%"', 'alert-info'))
+                            })
                         }
                         if (item.assessment.length > 4) {
-                            $('#rec_at').append(template.replace('%text%', item.assessment))
+                            let assessment = item.assessment;
+                            let assessments = assessment.split("\n").filter(function(el) {
+                                return el.length > 0;
+                            });
+
+                            assessments.forEach((assessment) => {
+                                $('#rec_at').append(template
+                                    .replace('%text%', assessment)
+                                    .replace('%alert_type%"', 'alert-info'))
+                            })
+
+
+                        }
+                        if (item.improvement.length > 4) {
+                            $('#improvement').append(template.replace('%text%', item.improvement).replace('%alert_type%"', 'alert-warning'))
                         }
                     })
 
 
-                    if($('#rec_form').children().length > 0) {
-                      $('#hasFormation').show()
+                    if ($('#rec_form').children().length > 0) {
+                        $('#hasFormation').show()
                     } else {
-                      $('#hasFormation').hide()
+                        $('#hasFormation').hide()
                     }
 
-                    if($('#rec_at').children().length > 0) {
-                      $('#hasAssessment').show()
+                    if ($('#rec_at').children().length > 0) {
+                        $('#hasAssessment').show()
                     } else {
-                      $('#hasAssessment').hide()
+                        $('#hasAssessment').hide()
+                    }
+
+                    if ($('#improvement').children().length > 0) {
+                        $('#hasImprovement').show()
+                    } else {
+                        $('#hasImprovement').hide()
                     }
 
 
